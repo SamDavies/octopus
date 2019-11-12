8de036d2a630cab0aa4b41e2c76c3277ca5e1835
Alex Litel <12000084+alexlitel@users.noreply.github.com>
2019-11-12 14:08:19 -0800

    Remove static page components

--- src/components/need_refactor/ui/atomic/atoms/Anchor/stories.js
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from '@storybook/react'
-import React from 'react'
-import Anchor from '.'
-
-storiesOf('components/elements/Anchor', module)
-    .add('Default', () =>
-        <Anchor href='/'>Testing Anchor</Anchor>)
