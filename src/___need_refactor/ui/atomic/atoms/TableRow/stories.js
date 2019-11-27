19b8ac9bad787ac241aa1ef8882c208df66981e2
Alex Litel <12000084+alexlitel@users.noreply.github.com>
2019-11-27 15:35:06 -0800

    Add table and move docs components

--- src/___need_refactor/ui/atomic/atoms/TableRow/stories.js
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from '@storybook/react'
-import React from 'react'
-import TableRow from './TableRow'
-
-storiesOf('components/elements/TableRow', module)
-    .add('Default', () =>
-        <TableRow>Hello</TableRow>)
--- src/stories/Button.js
+++ src/stories/Button.js
@@ -1,8 +1,9 @@
 import { storiesOf } from '@storybook/react'
 import React, { useState } from 'react'
 import { MemoryRouter } from 'react-router-dom'
-import BlockContainer from '../components/docs/BlockContainer'
-import Search from '../components/Search'
+import Select from '../components/Select'
+import Table from '../components/Table'
+
 // import CloseButton from '../components/___need_refactor/ui/atomic/atoms/CloseButton'
 
 // const Wrapper = styled.div`
@@ -30,30 +31,160 @@ import Search from '../components/Search'
 //     margin: auto;
 // `
 
+const columns = [
+    {
+        id: 'first',
+        label: 'First',
+        accessor: 'first'
+    },
+    {
+        id: 'second',
+        label: 'second',
+        cool: false,
+        accessor: 'second'
+    },
+    {
+        id: 'blah.cool.no',
+        label: 'yeah',
+        cool: false,
+        foo: true,
+        accessor: 'blah.cool.no'
+    }
+]
 
-const InputContainer = ({exist = '', inputProps = {}}) => {
-    const [value, setValue] = useState(exist)
-    return (
-        <BlockContainer id="blah">
-            <Search value={value} onChange={e => setValue(e.target.value)} />
-        </BlockContainer>
-    )
-}
+const data = [
+    {
+        id: '123',
+        first: 'blah',
+        second: 'no?',
+        blah: {
+            cool: {
+                no: 25
+            }
+        }
+    },
+    {
+        id: '1234',
+        first: 'blah2',
+        second: 'what',
+        blah: {
+            cool: {
+                no: null
+            }
+        }
+    },
+    {
+        id: '12345',
+        first: 'blah23',
+        second: 'what',
+        blah: {
+            cool: {
+                no: null
+            }
+        }
+    }
+]
+
+
+// const InputContainer = ({exist = '', inputProps = {}}) => {
+//     const [value, setValue] = useState(exist)
+//     return (
+//         <BlockContainer id="blah">
+//             <Search value={value} onChange={e => setValue(e.target.value)} />
+//         </BlockContainer>
+//     )
+// }
 
 storiesOf('components/Button', module)
 
-    .add('Normal disalbed2', () => (
+    .add('Normal disalbed2', () => {
+
+        // const newcolumns = React.useMemo(() => columns)
+        // const newdata = React.useMemo(() => data)
+        
+        const [newData, setData] = useState(data)
+        return (
+
+
         <MemoryRouter>
-        <div style={{
-            width: '500px'
-        }}>
-        <InputContainer />
+        <div>
+            <button onClick={() => {
+                setData(
+                    [...newData,
+                        {
+                            id: Math.random(),
+                            first: 'blah23' + Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5),
+                            second: 'what',
+                            blah: {
+                                cool: {
+                                    no: null
+                                }
+                            }
+                        }
+                    ]
+                )
+            }}>click me</button>
+            
+            <Table columns={columns}
+             data={newData}
+                title="cool?"
+                hasSearch
+                options={[
+                    {
+                        name: 'foo',
+                        id: 'what',
+                        onClick: (a,b,c) => {
+                            console.log(a,b,c)
+                        }
+                    }
+                ]}
+                customComponents={{
+                    cell: (cellPath, row, value, column) => {
+                        if (cellPath.includes('blah')) {
+                            return <Select
+                                selectType="secondary"
+                                label="cool?"
+                                hide
+                                options={[{
+                                    name: 'i great?',
+                                    value: 'cool',
+                                    id: '1323'
+                                }]}
+                            />
+                        }
+                        return <>{value}</>
+                    }
+                }}
+            />
         </div>
         </MemoryRouter>
-    ))
+        )
+    })
     // .add('Normal 2', () => (
     //     <MemoryRouter>
-            
+    //     <BlockContainer>
+    //         <Search
+    //             hasDropdown={false}
+    //             // value=[]
+    //             name="search"
+    //             onInputChange={e => {
+    //                 const inputVal = e.target.value
+    //                 // setValue(inputVal)
+    //             }}
+    //         />
+    //         </BlockContainer>
+    //     <BlockContainer>
+    //         <Search
+    //             // hasDropdown={false}
+    //             // value=[]
+    //             name="search"
+    //             onInputChange={e => {
+    //                 const inputVal = e.target.value
+    //                 // setValue(inputVal)
+    //             }}
+    //         />
+    //         </BlockContainer>
+
     //     </MemoryRouter>
     // ))
     // .add('Normal disalbed', () => (
